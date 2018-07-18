d5e069a52f50c5e44a5faa7aacf85c7e133b4f5f
gscho <greg.c.schofield@gmail.com>
2018-07-17 20:58:31 -0500

    Remove app dir

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -3,15 +3,14 @@ pkg_origin=gscho
 pkg_version="0.1.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("MIT")
-pkg_deps=(core/ruby core/curl)
+pkg_deps=(core/ruby25 core/curl)
 pkg_build_deps=(core/bundler)
 
 do_build(){
-  pushd ./app
-    bundle install
-  popd
+  return 0
 }
 
 do_install(){
-  cp -R ./app "$pkg_prefix/"
+  bundle install --path ./vendor/bundle
+  cp -R . "$pkg_prefix/"
 }
